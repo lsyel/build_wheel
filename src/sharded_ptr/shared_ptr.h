57bb8cdcfe5d5c5908d34667675aebd9380d9752
wzh <289410265@qq.com>
2025-09-09 14:30:20 +0800

    fix bug

--- src/sharded_ptr/shared_ptr.h
+++ src/sharded_ptr/shared_ptr.h
@@ -1,5 +1,4 @@
 #pragma once
-#include <cstddef>
 template <typename T>
 class shared_ptr
 {
@@ -21,7 +20,7 @@ private:
 
 public:
     shared_ptr() : ptr_(nullptr), ref_count_(nullptr) {};
-    explicit shared_ptr(T *ptr) : ptr_(ptr), ref_count_(new size_t(1)) {}
+    explicit shared_ptr(T *ptr) : ptr_(ptr), ref_count_(ptr? new size_t(1): nullptr) {}
 
     shared_ptr(const shared_ptr &other) : ptr_(other.ptr_), ref_count_(other.ref_count_)
     {
@@ -31,10 +30,10 @@ public:
         }
     }
 
-    shared_ptr(const shared_ptr &&other) noexcept : ptr_(other.ptr_), ref_count_(other.ref_count_)
+    shared_ptr( shared_ptr &&other) noexcept : ptr_(other.ptr_), ref_count_(other.ref_count_)
     {
-        ptr_ = nullptr;
-        ref_count_ = nullptr;
+        other.ptr_ = nullptr;
+        other.ref_count_ = nullptr;
     }
 
     shared_ptr &operator=(const shared_ptr &other)
@@ -44,14 +43,14 @@ public:
             release();
             ptr_ = other.ptr_;
             ref_count_ = other.ref_count_;
-            if (*ref_count_)
+            if (ref_count_)
             {
                 (*ref_count_)++;
             }
         }
-        return this;
+        return *this;
     }
-    shared_ptr &operator=(const shared_ptr &&other) noexcept
+    shared_ptr &operator=(shared_ptr &&other) noexcept
     {
         if (this != &other)
         {
@@ -107,19 +106,6 @@ public:
         ref_count_ = p ? new size_t(1) : nullptr;
     }
 
-    explicit operator bool()
-    {
-        return ptr_ != nullptr;
-    }
-
-    bool operator==(const shared_ptr &other)
-    {
-        return ptr_ == other.ptr_;
-    }
-    bool operator!=(const shared_ptr &other)
-    {
-        return ptr_ != other.ptr_;
-    }
 };
 
 template <typename T, typename... Args>
