85e0623d7603c4935f89db64e0eb7930edcb314d
wzh <1190300107@stu.hit.edu.cn>
2025-09-20 18:08:06 +0800

    chinese

---
+++ src/chinese/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#define 定义 int
+#define 等于 =
+#define 求和(x,y) ((x)+(y))
+#define 打印(x) std::cout<<#x <<"等于:"<<x<<std::endl
+#define 五 5
+#define 六 6
+#define 程序 main
+#define 程序结束 return 0
+#define 定义一个程序 定义 程序
+定义一个程序(){
+    定义 变量一 等于 五;
+    定义 变量二 等于 六;
+    定义 变量三 等于 求和(变量一,变量二);
+    打印(变量三);
+    程序结束;
+}
